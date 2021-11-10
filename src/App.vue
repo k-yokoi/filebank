50819a7a278570d8711990911ed3987f85a063cd
Kazuki Yokoi <kazuki.yokoi11@gmail.com>
2021-11-10 20:22:40 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+      <amplify-authenticator username-alias="email">
+        <amplify-sign-up
+      slot="sign-up"
+      username-alias="email"
+    ></amplify-sign-up>
+    <amplify-sign-in slot="sign-in" username-alias="email"></amplify-sign-in>
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  </amplify-authenticator>
   </div>
 </template>
 
@@ -14,15 +22,160 @@ export default {
     HelloWorld
   }
 }
+
 </script>
 
+
+
 <style>
+body {
+  margin: 0;
+  background-color: white;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align:center;
+}
+h4 {
+  font-weight: 400;
+  text-align: left;
+}
+h5 {
+  font-weight: 400;
+  text-align: left;
+}
+.pre {
+  background: #f8f8f8
+}
+.container {
+  text-align: center;
+  margin-left: 30px;
+}
+.shifted {
+  text-align: left;
+}
+section {
+  margin-top: 30px;
+}
+article {
+  margin-top: 30px;
+}
+.lineBreak {
+  height: 0;
+  border-bottom: 1px solid #ededed;
+  margin: 1em 10%;
+}
+.form {
+  display: inline-block;
+  width: 320px;
+}
+inputRow {
+  display: block;
+  margin-top: 0.5em;
+}
+.input {
+  width: 100%;
+  font-size: 1em;
+  padding: 0.5em;
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+  border-bottom: 2px solid #2196F3;
+}
+.inputLabel {
+  display: block;
+  text-align: left;
+}
+.actionRow {
+  display: flex;
+  margin-top: 0.5em;
+  text-align: right;
+}
+.action {
+  font-size: 1em;
+  border: none;
+  outline: none;
+  padding: 12px 50px;
+  background-color: #ededed;
+  cursor: pointer;
+}
+.footer {
+  margin-top: 1em;
+}
+.footerLeft {
+  float: left;
+}
+.footerRight {
+  float: right;
+}
+.link {
+  text-decoration: underline;
+  cursor: pointer;
+}
+.error {
+  background: orange;
+  padding: 0.5em;
+}
+.nav {
+  margin-bottom: 1em;
+  display: flex;
+  justify-content: space-between;
+  background-color: #2196F3;
+  color: #fff;
+  padding: 1em;
+}
+.item {
+  font-weight: 500;
+  padding: 0 0.5em;
+  cursor: pointer;
+}
+.nav > .greeting {
+  font-weight: 500;
+  color: #000;
+  padding: 0 0.5em;
+}
+.nav > .main {
+  flex-grow: 1;
+  text-align: left;
+}
+.nav >.right {
+  font-size: 0.8em;
+}
+.avatar {
+  max-height: 150px;
+}
+.avatar-pic {
+  max-height: 100px;
+}
+/* accordion */
+.message {
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.message-header {
+  cursor: pointer;
+}
+.message-body   {
+  padding: 0;
+  max-height: 50em;
+  width: 100%;
+  overflow: hidden;
+  transition: 0.3s ease all;
+}
+.is-closed .message-body {
+  max-height: 0;
+}
+.is-primary .arrow {
+  transform: rotateZ(90deg) translatex(-5px);
+}
+.arrow {
+  display: inline-block;
+}
+.message-content {
+  padding: 20px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,156 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="=container">
+          <div class="nav">
+    <div class="main">
+    </div>
+    <div class="right">
+      <el-button @click="signout" >SIGN OUT</el-button>
+    </div>
+  </div>
+  
+  <div class="container shifted">
+
+    <h1 class="h1">
+      FILE BANK
+    </h1>
+      
+    <el-button>
+      <label for="file">
+        Upload
+        <input type="file" @change="upload" id="file" style="display:none;">
+      </label>
+    </el-button>
+    <el-button @click="refresh" class="el-icon-refresh-left"></el-button>
+    <el-table 
+      :data="s3Data"
+      style="width: 100%">
+      <el-table-column
+        prop="key"
+        label="Key"
+        sortable>
+      </el-table-column>
+      <el-table-column
+        prop='lastModified'
+        label="LastModified"
+        sortable>
+      </el-table-column>
+      <el-table-column
+        prop="size"
+        label="Size"
+        sortable>
+      </el-table-column>
+      <el-table-column>
+        <template slot-scope="scope">
+          <el-button
+            @click="download(scope.row)">Download</el-button>
+          <el-button type="danger" @click="openDeleteDialog(scope.row)">Delete</el-button>
+        </template>
+      </el-table-column>    
+    </el-table>
+    <el-dialog
+      title="Delete objects"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>{{ deleteObject }} Objects will be deleted</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="deleteOK()">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { Auth, Storage } from 'aws-amplify';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, { locale })
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'Home',
+  data () {
+    return {
+      s3Data: [],
+      dialogVisible: false,
+      selectedRow: "",
+      deleteObject: "",
+      uploadFile: ""
+    }
+  },
+  created () {
+    this.refresh()  
+  },
+  methods: {
+    refresh () {
+      Storage.list('', { level: 'private' })
+        .then(result => this.s3Data = JSON.parse(JSON.stringify(result)))
+        .catch(err => console.log(err));
+    },
+    upload(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      console.log(files)
+      Storage.put(files[0].name, files[0], {level: "private"})
+        .then(result => {
+          console.log(result)
+          this.refresh() 
+          })
+        .catch(err => console.log(err));
+    },
+    download (row) {
+      Storage.get(row['key'], { level: "private", download: true })
+        .then(result => {
+          console.log(result)
+          const url = URL.createObjectURL(new Blob([result.Body]));
+          const link = document.createElement('a')
+          link.href = url
+          link.download = row['key']
+          console.log(link)
+          link.click()
+        })
+        .catch(err => console.log(err));
+    },
+    openDeleteDialog(row) {
+      this.selectedRow = row;
+      console.log(row)
+      this.deleteObject = row['key'];
+      this.dialogVisible = true
+    },
+    deleteOK () {
+      this.dialogVisible = false
+      Storage.remove(this.selectedRow['key'])
+        .then(result => {
+          console.log(result)
+          this.refresh() 
+          }
+        )
+        .catch(err => console.log(err));
+    },      
+    handleClose(done) {
+        this.$confirm('Are you sure to close this dialog?')
+          .then(done())
+          .catch();
+    },
+    signout: async function() {
+      try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+    },
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+label {
+  color: #606266;  
+  background-color:white;
+  padding: 10px;
 }
 </style>
